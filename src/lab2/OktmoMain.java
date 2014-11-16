f0043b4f496b1802de42f4e57e5759f580b7f6a4
nailakmurzin <nailakmurzin@yandex.ru>
2014-11-16 19:00:57 +0500

    dsfds

---
+++ src/lab2/OktmoMain.java
@@ -0,0 +1,9 @@
+package lab2;
+
+public class OktmoMain {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ src/lab2/Place.java
@@ -0,0 +1,9 @@
+package lab2;
+
+public class Place {
+    private long code = 0;
+    private String name = "";
+    private String status = "";
+    
+    
+}
